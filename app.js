faf98a6077b945444c5528ec5eb85427538bf28e
Kirill Dolzhenkov <k.dolzhenkov@gmail.com>
2021-09-23 02:07:46 +0300

    refresh cards slyle

--- app.js
+++ app.js
@@ -2,14 +2,13 @@ const slides = document.querySelectorAll('.slide');
 
 for (const slide of slides) {
     slide.addEventListener('click', () => {
-        clearActiveClasses()
-
-        slide.classList.add('active')
-    })
+        clearActiveClasses();
+        slide.classList.add('active');
+    });
 }
 
 const clearActiveClasses = () => {
     slides.forEach((slide)=>{
-        slide.classList.remove('active')
-    })
+        slide.classList.remove('active');
+    });
 }
